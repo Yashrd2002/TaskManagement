2368fe311dc0b2039c749b3f20501797db0dadff
Yash2002 <yashrajiv.deshmukh2021@vitstudent.ac.in>
2025-02-20 22:43:28 +0530

    first commit

--- server/main.go
+++ server/main.go
@@ -16,7 +16,7 @@ func main() {
 	r := gin.Default()
 
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173", "https://your-production-frontend.com", "http://localhost:3001"},
+		AllowOrigins:     []string{"http://localhost:5173", "https://task-management-mu-hazel.vercel.app", "http://localhost:3001"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Content-Type", "Authorization", "X-Requested-With", "Accept"},
 		ExposeHeaders:    []string{"Content-Length", "Content-Type", "Authorization"},
